92a2d479a2ad87b264ae81c381f72023e7419607
Kristian Ivanov <kriskopk@abv.bg>
2023-08-31 12:32:57 +0300

    Header component done.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main >
-      <h1 className="font-bold text-3xl">Hello</h1>
       {/* Header */}
       <Header />
       {/* Board */}
+      <Board />
     </main>
   );
 }
