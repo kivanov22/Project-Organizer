c1df990d4c5dccda720ccc5693109034835b4a4a
Kristian <krisko512@gmail.com>
2023-10-06 21:08:43 +0300

    Implementing Drag n Drop functionality.

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,14 +1,42 @@
-import React from 'react'
+"use client";
 
-type Props ={
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from "react-beautiful-dnd";
 
-}
+type Props = {
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+};
 
-
-const TodoCard = ({}:Props) => {
+const TodoCard = ({
+  todo,
+  index,
+  id,
+  innerRef,
+  dragHandleProps,
+  draggableProps,
+}: Props) => {
   return (
-    <div>TodoCard</div>
-  )
-}
+    <div className="bg-white rounded-md space-y-2 drop-shadow-md"
+    {...draggableProps} {...dragHandleProps} ref={innerRef}
+    >
+      <div className="flex justify-between items-center p-5">
+        <p>{todo.title}</p>
+        <button className="text-red-500 hover:text-red-600">
+          <XCircleIcon className="ml-5 h-8 w-8"/>
+        </button>
+      </div>
 
-export default TodoCard
+      
+    </div>
+  );
+};
+
+export default TodoCard;
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,17 +1,32 @@
+import { databases } from '@/appwrite';
 import { getTododsGroupedByColumn } from '@/lib/getTodosGroupedByColumn';
 import { create } from 'zustand'
 
 interface BoardState {
-    board:Board;
-    getBoard:()=>void;
+  board: Board;
+  getBoard: () => void;
+  setBoardState: (board: Board) => void;
+  updateTodoInDb: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
   board: {
-    columns: new Map<TypedColum,Column>()
+    columns: new Map<TypedColumn, Column>()
   },
-  getBoard: async()=>{
+  getBoard: async () => {
     const board = await getTododsGroupedByColumn();
-    set({board})
-  }
-}))
+    set({ board })
+  },
+  setBoardState: (board) => set({ board }),
+  updateTodoInDb:async(todo, columnId)=> {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title:todo.title,
+        status:columnId
+      }
+    )
+  },
+}));
